e653a27f148c1fad5d6d3ed7ca75c2d3da653b60
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2019-04-22 23:40:13 -0400

    Atualização métodos Login e Logout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Tymon\JWTAuth\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
 
 class AuthController extends Controller
 {
@@ -13,13 +14,21 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        $this->validateLogin($request);
 
-        $credentials = $this->credentials($request);
+        $credentials = $request->only('email', 'password');
 
-        $token = \JWTAuth::attempt($credentials);
+        try {
+            $token = \JWTAuth::attempt($credentials);
+        } catch (JWTException $exception) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
 
-        return $this->responseToken($token);
+        if (!$token) {
+            return response()->json(['error' => 'invalid_credentials'], 401);
+        }
+
+        return response()->json(compact('token'));
+
     }
 
     private function responseToken($token)
@@ -32,8 +41,13 @@ class AuthController extends Controller
 
     public function logout()
     {
-        \Auth::guard('api')->logout();
-        return response()->json([], 204); //no-content
+        try {
+            \JWTAuth::invalidate();
+        } catch (JWTException $exception) {
+            return response()->json(['error' => 'could_not_invalidate_token'], 500);
+        }
+        return response()->json([], 204);
+
     }
 
     public function refresh()
--- routes/api.php
+++ routes/api.php
@@ -7,15 +7,7 @@ use Illuminate\Http\Request;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::post('login', 'Api\AuthController@login')->name('api.login');
 Route::post('refresh', 'Api\AuthController@refresh')->name('api.refresh');
