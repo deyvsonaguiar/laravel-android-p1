cc5b26a098bf0bb2ace566bd9eff31a3ac020765
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2019-04-25 21:51:43 -0400

    Criando repository Category

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,9 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Models\Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name
+    ];
+});
