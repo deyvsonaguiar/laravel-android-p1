f65504eda70affe4fae550b0da47f519549b1d2e
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2019-04-26 22:00:32 -0400

    Listagem de Categorias e correção de erros CategoryController

--- routes/api.php
+++ routes/api.php
@@ -9,12 +9,18 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::post('login', 'Api\AuthController@login')->name('api.login');
-Route::post('refresh_token', 'Api\AuthController@refreshToken')->name('api.refresh_token');
+Route::group(['namespace' => 'Api'], function() {
 
-Route::group(['middleware' => ['auth:api']], function() {
-    Route::get('users', function () {
-        return \App\User::all();
+    Route::post('login', 'AuthController@login')->name('api.login');
+    Route::post('refresh_token', 'AuthController@refreshToken')->name('api.refresh_token');
+
+    Route::group(['middleware' => ['auth:api']], function() {
+        Route::get('users', function () {
+            return App\Models\User::all();
+        });
+        Route::post('logout', 'AuthController@logout')->name('api.logout');
+        Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
     });
-    Route::post('logout', 'Api\AuthController@logout')->name('api.logout');
+
 });
+
