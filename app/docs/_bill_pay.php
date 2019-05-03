a76ad5a33f931f2a8bf9efb407710989158bfade
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2019-05-03 15:26:37 -0400

    Atualizando documentação swagger _bill_pays

--- app/docs/_bill_pay.php
+++ app/docs/_bill_pay.php
@@ -20,21 +20,74 @@
  *                      @OA\Items(
  *                          @OA\Property(property="count_paid", type="integer"),
  *                          @OA\Property(property="total_paid", type="integer", format="float")
- *                      )
- *                  )
- *              ),
- *              @OA\Schema(
+ *                      )          
+ *                  ),
  *                  @OA\Property(
- *                      property="about",
+ *                      property="data",
  *                      type="array",
  *                      @OA\Items(
- *                          @OA\Property(property="count_paid", type="integer"),
- *                          @OA\Property(property="total_paid", type="integer", format="float")
- *                      )
- *                  )
+ *                          @OA\Property(property="id", type="integer"),
+ *                          @OA\Property(property="name", type="string"),
+ *                          @OA\Property(
+ *                              property="date_due", 
+ *                              type="array",
+ *                              @OA\Items(
+ *                                  @OA\Property(property="date", type="string", format="date-time"),
+ *                                  @OA\Property(property="timezone_type", type="integer"),
+ *                                  @OA\Property(property="timezone", type="string"),
+ *                              ) 
+ *                          ),
+ *                          @OA\Property(property="value", type="float"),
+ *                          @OA\Property(property="done", type="boolean"),
+ *                          @OA\Property(property="user_id", type="integer"),
+ *                          @OA\Property(
+ *                              property="category_id", 
+ *                              type="array",
+ *                              @OA\Items(
+ *                                  @OA\Property(property="id", type="integer"),
+ *                                  @OA\Property(property="name", type="string"),
+ *                                  @OA\Property(
+ *                                      property="created_at", 
+ *                                      type="array",
+ *                                          @OA\Items(
+ *                                              @OA\Property(property="date", type="string", format="date-time"),
+ *                                              @OA\Property(property="timezone_type", type="integer"),
+ *                                              @OA\Property(property="timezone", type="string"),
+ *                                          ) 
+ *                                  ),
+  *                                  @OA\Property(
+ *                                      property="updated_at", 
+ *                                      type="array",
+ *                                          @OA\Items(
+ *                                              @OA\Property(property="date", type="string", format="date-time"),
+ *                                              @OA\Property(property="timezone_type", type="integer"),
+ *                                              @OA\Property(property="timezone", type="string"),
+ *                                          ) 
+ *                                  ),
+ *                              ) 
+ *                          ),
+ *                          @OA\Property(
+ *                                      property="created_at", 
+ *                                      type="array",
+ *                                          @OA\Items(
+ *                                              @OA\Property(property="date", type="string", format="date-time"),
+ *                                              @OA\Property(property="timezone_type", type="integer"),
+ *                                              @OA\Property(property="timezone", type="string"),
+ *                                          ) 
+ *                                  ),
+  *                                  @OA\Property(
+ *                                      property="updated_at", 
+ *                                      type="array",
+ *                                          @OA\Items(
+ *                                              @OA\Property(property="date", type="string", format="date-time"),
+ *                                              @OA\Property(property="timezone_type", type="integer"),
+ *                                              @OA\Property(property="timezone", type="string"),
+ *                                          ) 
+ *                                  ),
+ *                          )          
+ *                  ),
  *              )
-*           )
- *       )
- *    )
+ *          )
+ *     )
  * )
  */
